41e072f8f5d1352fe4e1dc302a29fc37daf64677
WoongJae Han <happyhj6@gachon.ac.kr>
2025-05-22 23:36:28 +0900

    JoinEventActivity에 간단한 주석설명추가

--- app/src/main/java/com/platinaace/forestoftime/App.java
+++ app/src/main/java/com/platinaace/forestoftime/App.java
@@ -8,10 +8,14 @@ public class App extends Application {
     public void onCreate() {
         super.onCreate();
         Parse.initialize(new Parse.Configuration.Builder(this)
-                .applicationId("8zhtT1Ehpty5MM3oan8bcOsjc74jBhZ9tsZoh4r3")       // from Back4App
-                .clientKey("BMsZb7ahr4BR3oQMgSP3VpvQCfQeCEmwxvFZLISi")       // from Back4App
+                .applicationId("QhsuNJHFf15d3TBBRNrd7Hfk9nEg9He73P0slgqU")       // from Back4App
+                .clientKey("pMC3PzI4Qru69Lwg4OOiJyTUL3BAzWHYR9sHFqbz")       // from Back4App
                 .server("https://parseapi.back4app.com/")
                 .build()
         );
     }
 }
+
+
+//8zhtT1Ehpty5MM3oan8bcOsjc74jBhZ9tsZoh4r3
+//BMsZb7ahr4BR3oQMgSP3VpvQCfQeCEmwxvFZLISi
